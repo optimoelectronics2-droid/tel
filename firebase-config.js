33a661d974ce86f430ebbf65c6a3fd1d17dabee4
optimoelectronics2-droid <optimoelectronics2@gmail.com>
2026-02-04 20:04:16 -0700

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,19 +1,10 @@
-// firebase-config.js
-// SDK compat (sirve en HTML puro, GitHub Pages, sin bundlers)
-
 const firebaseConfig = {
-  apiKey: "AIzaSyDlOJqBgd-8KqUT-Y94lsma5W8T79PsPjM",
-  authDomain: "trifusion-cotizador.firebaseapp.com",
-  projectId: "trifusion-cotizador",
-  storageBucket: "trifusion-cotizador.firebasestorage.app",
-  messagingSenderId: "407374211882",
-  appId: "1:407374211882:web:b692792ccc39c8d48f4c53"
+  apiKey: "TU_API_KEY",
+  authDomain: "TU_DOMINIO.firebaseapp.com",
+  projectId: "TU_PROJECT_ID",
 };
 
-// Inicializar Firebase (compat)
 firebase.initializeApp(firebaseConfig);
 
-// Accesos globales
-const db = firebase.firestore();
 const auth = firebase.auth();
-const storage = firebase.storage();
+const db = firebase.firestore();
